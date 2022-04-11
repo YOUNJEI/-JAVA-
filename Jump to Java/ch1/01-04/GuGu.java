8d59a12b6a8981cb30fc03225eeb09324dd7c0e2
YOUNJEI <kyj951211@gmail.com>
2022-04-11 23:26:07 +0900

    01-04

---
+++ Jump to Java/ch1/01-04/GuGu.java
@@ -0,0 +1,12 @@
+public class GuGu {
+	public static void dan(int n) {
+		for (int i = 1; i < 10; i++) {
+			System.out.println(n * i);
+		}
+	}
+	public static void main(String[] args){
+		GuGu gugu = new GuGu();
+		for (int i = 2; i < 10; i ++)
+			gugu.dan(i);
+	}
+}
