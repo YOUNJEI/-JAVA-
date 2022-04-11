2b3b603d9159929181289cdab2bc4e5d0dcc809f
YOUNJEI <kyj951211@gmail.com>
2022-04-12 01:43:37 +0900

    03-07

---
+++ Jump to Java/ch3/03-07 List/Sample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Sample
+{
+	public static void main(String[] args)
+	{
+		ArrayList<String> pitches = new ArrayList<>();
+		pitches.add("138");
+		pitches.add("129");
+		pitches.add("142");
+		System.out.println(pitches);
+		System.out.println(pitches.get(0));
+	}
+}
